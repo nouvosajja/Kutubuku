9826b067a61a5f30d28af5b231f03f405e1534e4
nouvosajja <nouvochrisnaa@gmail.com>
2025-07-02 13:00:19 +0700

    checking

--- User/Settings/setting.php
+++ User/Settings/setting.php
@@ -1,4 +1,15 @@
-<?php session_start(); ?>
+<?php
+session_start();
+
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1
+header("Pragma: no-cache"); // HTTP 1.0
+header("Expires: 0"); // Proxies
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: ../Login/loginU.php");
+    exit;
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
